1f9db41f31f411fd6c0202d0fe292411b1d82bc6
kiru-she12 <kirushika1211@gmail.com>
2025-11-07 23:39:16 +0530

    its pop right

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const myFirst = 250;
-console.log(myFirst);
+const myFirst = 250
+console.log(myFirst)
+
+const typeofmyFirst = (typeof myFirst)
+console.log(typeofmyFirst)
+let age = 10
+let sum = 200
+let totalcount = 5
+
+function add(increment,multiplier){
+   sum = sum + 100
+   return
+   totalcount = totalcount + 200
+}
+const newage = add(3,6)
+console.log(sum,totalcount)
+
+let myfriends =[]
+
+function addfriend(name){
+    myfriends.unshift(name)
+    console.log(myfriends)
+}
+addfriend("Alice")
+addfriend("Bob")
+addfriend("Charlie")
