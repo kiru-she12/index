f8b7573cb071a8c1f30309e5b6d37be8126d4e6c
kiru-she12 <kirushika1211@gmail.com>
2025-11-08 15:46:08 +0530

    functions in JS

--- index.js
+++ index.js
@@ -24,3 +24,35 @@ function addfriend(name){
 addfriend("Alice")
 addfriend("Bob")
 addfriend("Charlie")
+
+//equality
+const result = (5 === '5')
+console.log(result)
+
+//objects
+const myObj = {
+    keyname1: {
+        'keyname 2.1': 'hello',
+        obj2 : {
+           key: null 
+        }
+    }, 
+       
+    'keyname2': 'value2',
+    'keyname3': 'value3'
+}
+console.log(myObj['keyname1'])
+
+const myprofile = {
+    name : 'kiru',
+    age : 22
+}
+
+const secondProfile = {
+    name : 'She',
+    age : 33
+}
+function mutate(obj){
+    console.log(obj.age)
+}
+mutate(secondProfile)
